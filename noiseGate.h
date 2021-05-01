8a50e5086e999b33e57d6c7d24c7840ad7616c47
Wyatt Young <wcyoung46@gmail.com>
2021-04-30 23:29:48 -0700

    worked on noise gate header

--- noiseGate.h
+++ noiseGate.h
@@ -4,5 +4,17 @@
 #include "IProcessable.h"
 
 class NoiseGate : IProcessable{
-    
+
+	uint8_t threshold; 
+
+
+public:
+
+	void processBuffer(unsigned char *buffer, int bufferSize);
+
+	NoiseGate(uint8_t threshold);
+
+
 };
+
+#endif
